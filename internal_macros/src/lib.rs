e0b8faff5dd01c562ffc6998c478e32232726252
HP2706 <hprjdk@gmail.com>
2024-03-30 13:48:01 +0100

    name change of openaischema to openasischemaSpec+ no need for import when deriving  OpenAISchema

--- internal_macros/src/lib.rs
+++ internal_macros/src/lib.rs
@@ -3,7 +3,8 @@ extern crate proc_macro;
 use proc_macro::TokenStream;
 use quote::quote;
 use syn::{parse_macro_input, DeriveInput};
-use shared::OpenAISchema;
+
+
 
 #[proc_macro_derive(OpenAISchema)]
 pub fn schema_to_string_derive(input: TokenStream) -> TokenStream {
@@ -11,7 +12,7 @@ pub fn schema_to_string_derive(input: TokenStream) -> TokenStream {
 
     let name = &input.ident;
     let gen = quote! {
-        impl OpenAISchema for #name {
+        impl shared::OpenAISchemaSpec for #name {
             fn openai_schema() -> String {
                 let schema = schemars::schema_for!(#name);
                 serde_json::to_string_pretty(&schema).unwrap()
@@ -20,50 +21,50 @@ pub fn schema_to_string_derive(input: TokenStream) -> TokenStream {
             fn model_validate_json<T>(
                 data: &str,
                 validation_context: T::Args,
-            ) -> Result<T, Error>
+            ) -> Result<T, shared::enums::Error>
             where
-                T: ValidateArgs<'static> + Serialize + for<'de> Deserialize<'de>,
+                T: validator::ValidateArgs<'static> + serde::Serialize + for<'de> serde::Deserialize<'de>,
             {
                 match serde_json::from_str::<T>(data) {
                     Ok(data) => {
                         match data.validate_args(validation_context) {
                             Ok(_) => Ok(data),
-                            Err(e) => Err(Error::Validation(e)),
+                            Err(e) => Err(shared::enums::Error::Validation(e)),
                         }
                     }
-                    Err(e) => Err(Error::Validation(ValidationErrors::new())),
+                    Err(e) => Err(shared::enums::Error::Validation(validator::ValidationErrors::new())),
                 }
             }
 
             fn from_response<T>(
-                response: &ChatCompletionResponse,
+                response: &openai_api_rs::v1::chat_completion::ChatCompletionResponse,
                 validation_context: T::Args,
-                mode: Mode,
-            ) -> Result<T, Error>
+                mode: shared::mode::Mode,
+            ) -> Result<T, shared::enums::Error>
             where
-                T: ValidateArgs<'static> + Serialize + for<'de> Deserialize<'de> + OpenAISchema,
+                T: validator::ValidateArgs<'static> + serde::Serialize + for<'de> serde::Deserialize<'de> + shared::OpenAISchemaSpec,
             {
                 match mode {
-                    Mode::JSON | Mode::JSON_SCHEMA | Mode::MD_JSON => {
+                    shared::mode::Mode::JSON | shared::mode::Mode::JSON_SCHEMA | shared::mode::Mode::MD_JSON => {
                         Self::parse_json(response, validation_context)
                     }
                     _ => {
                         return Err(
-                            Error::NotImplementedError("This feature is not yet implemented.".to_string())
+                            shared::enums::Error::NotImplementedError("This feature is not yet implemented.".to_string())
                         );   
                     }
                 }
             }
 
             fn parse_json<T>(
-                completion : &ChatCompletionResponse,
+                completion : &openai_api_rs::v1::chat_completion::ChatCompletionResponse,
                 validation_context: T::Args,
-            ) -> Result<T, Error>
+            ) -> Result<T,shared::enums::Error>
             where
-                T: ValidateArgs<'static> + Serialize + for<'de> Deserialize<'de> + OpenAISchema,
+                T: validator::ValidateArgs<'static> + serde::Serialize + for<'de> serde::Deserialize<'de> + shared::OpenAISchemaSpec,
             {
                 let text = completion.choices[0].message.content.clone().unwrap();
-                let json_extract = extract_json_from_codeblock(&text);
+                let json_extract = shared::utils::extract_json_from_codeblock(&text);
                 T::model_validate_json(&json_extract, validation_context)
             }
 
--- shared/src/lib.rs
+++ shared/src/lib.rs
@@ -8,7 +8,7 @@ use validator::{ValidateArgs, ValidationErrors};
 use serde::{Deserialize, Serialize};
 use openai_api_rs::v1::chat_completion::ChatCompletionResponse;
 use crate::enums::Error;
-pub trait OpenAISchema {
+pub trait OpenAISchemaSpec {
     fn openai_schema() -> String;
 
     fn model_validate_json<T>(
@@ -25,14 +25,14 @@ pub trait OpenAISchema {
         mode: Mode,
     ) -> Result<T, Error>
     where
-        T: ValidateArgs<'static> + Serialize + for<'de> Deserialize<'de> + OpenAISchema;
+        T: ValidateArgs<'static> + Serialize + for<'de> Deserialize<'de> + OpenAISchemaSpec;
 
     fn parse_json<T>(
         completion : &ChatCompletionResponse,
         validation_context: T::Args,
     ) -> Result<T, Error>
     where
-        T: ValidateArgs<'static> + Serialize + for<'de> Deserialize<'de> + OpenAISchema;
+        T: ValidateArgs<'static> + Serialize + for<'de> Deserialize<'de> + OpenAISchemaSpec;
 
 
     //TODOS
